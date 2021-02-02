a4a76a2f0fcf451dce4b4f02a7559d153d286311
Witoslaw Koczewski <wi@koczewski.de>
2021-02-02 21:19:35 +0100

    react

--- src/spark/react.cljs
+++ src/spark/react.cljs
@@ -1,5 +1,7 @@
 (ns spark.react
-  (:require-macros [spark.react :refer [use-state use-effect]]))
+  (:require-macros [spark.react :refer [use-state use-effect]])
+  (:require
+   [helix.hooks :as helix-hooks]))
 
 
 (defn atom-hook
@@ -7,7 +9,7 @@
    (atom-hook ATOM identity))
   ([ATOM transformator]
    (fn use-atom []
-     (let [[value set-value] (use-state @ATOM)
+     (let [[value set-value] (helix-hooks/use-state @ATOM)
            watch-key (random-uuid)]
 
        (use-effect
