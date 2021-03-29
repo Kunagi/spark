72dcc371b410518a8589401cd5e752a33ba77584
Witoslaw Koczewski <wi@koczewski.de>
2021-03-29 12:48:53 +0200

    utils: index-by

--- src/spark/utils.cljs
+++ src/spark/utils.cljs
@@ -43,6 +43,14 @@
     m
     (assoc m k v)))
 
+(defn index-by
+  "Returns a map of the elements of coll keyed by the result of f on each
+  element. The value at each key will be the last element in coll associated
+  with that key. This function is similar to `clojure.core/group-by`, except
+  that elements with the same key are overwritten, rather than added to a
+  vector of values."
+  [f coll]
+  (persistent! (reduce #(assoc! %1 (f %2) %2) (transient {}) coll)))
 
 ;;; vectors
 
