7d319381f2c5d4a343ba09ddd4e60efe3d7db6f3
Witoslaw Koczewski <wi@koczewski.de>
2021-07-21 14:34:02 +0200

    firebase backup

--- src/spark/firebase/backup.cljs
+++ src/spark/firebase/backup.cljs
@@ -5,9 +5,18 @@
    [tick.alpha.api :as tick]
    [tick.format :as tick.format]
    ["firebase-admin" :as admin]
+
+   [spark.logging :refer [log]]
    [spark.firestore :as firestore]
    [spark.utils :as u]
    [spark.gcf :as gcf]))
+
+;; https://firebase.google.com/docs/storage/admin/start
+;; https://firebase.google.com/docs/reference/admin/node/admin.storage
+
+;; https://googleapis.dev/nodejs/storage/latest/Bucket.html
+(defn bucket [bucket-name]
+  (-> ^js admin .storage (.bucket (str "gs://" bucket-name))))
 
 (defn file [bucket path]
   (-> ^js bucket (.file path)))
@@ -27,6 +36,11 @@
        (-> writable ^js .end)
        ))))
 
+(comment
+  (-> (bucket "happygast-backup") .-acl .get)
+  (u/=> (write-string> (bucket "happygast-backup") "dummy.txt" "42")
+        u/tap>))
+
 (defn download-string [bucket path]
   (let [file (file bucket path)]
     (-> ^js file .download)))
@@ -36,14 +50,38 @@
         s    (u/->edn doc)]
     (write-string> bucket path s)))
 
+(defn- write-next-doc> [bucket path docs results]
+  (if-let [doc (first docs)]
+    (u/=> (write-doc> bucket path doc)
+          (fn [result]
+            (write-next-doc> bucket path (rest docs) (conj results result))))
+    (u/as> results))
+  )
+
 (defn backup-col> [bucket path col-name]
+  (log ::backup-col>
+       :bucket bucket
+       :path path
+       :col col-name)
   (u/=> (firestore/col> [col-name])
-        #(u/all> (map (partial write-doc> bucket path) %))))
+        (fn [docs]
+          (write-next-doc> bucket path docs [])
+          #_(u/all> (map (partial write-doc> bucket path) docs))
+          )))
+
+(defn- backup-next-col> [bucket path cols-names results]
+  (if-let [col-name (first cols-names)]
+    (u/=> (backup-col> bucket path col-name)
+          (fn [result]
+            (backup-next-col> bucket path (rest cols-names) (conj results result))))
+    (u/as> results)))
 
 (defn backup-cols> [bucket path cols-names]
-  (u/all>
-   (map #(backup-col> bucket path %)
-        cols-names)))
+  (backup-next-col> bucket path cols-names [])
+  #_(u/all>
+     (map #(backup-col> bucket path %)
+          cols-names))
+  )
 
 (def date-path-format (tick.format/formatter "yyyy/MM/dd"))
 
@@ -55,20 +93,17 @@
      "/"
      (tick/now))))
 
-(defn bucket [bucket-name]
-  (-> ^js admin .storage (.bucket (str "gs://" bucket-name))))
-
 (defn backup-all> [bucket-name]
+  (log ::backup-all>
+       :bucket bucket-name)
   (let [bucket (bucket bucket-name)
-        path   (str (if goog.DEBUG "dev" "prod")
-                    "/"
-                    (date-path))]
+        path   (date-path)]
     (u/=> (firestore/cols-names>)
           (fn [cols-names]
             (backup-cols> bucket path cols-names)))))
 
 (comment
-  (-> (backup-all> "legilo-backups")
+  (-> (backup-all> "happygast-backup")
       u/tap>))
 
 
