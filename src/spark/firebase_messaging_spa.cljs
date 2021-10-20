b0c1d8e4cfdd3c005f80cdd249835f78aecf06f1
Witoslaw Koczewski <wi@koczewski.de>
2021-10-20 14:14:39 +0200

    [bugfix]

--- src/spark/firebase_messaging_spa.cljs
+++ src/spark/firebase_messaging_spa.cljs
@@ -1,16 +1,20 @@
 (ns spark.firebase-messaging-spa
   (:require
-   [spark.logging :refer [log]]))
+   [spark.logging :refer [log]]
+   [spark.utils :as u]))
 
 (defn messaging []
-  (-> js/firebase .messaging))
+  (when (exists? js/firebase.messaging)
+    (-> js/firebase .messaging)))
 
 ;; https://firebase.google.com/docs/cloud-messaging/js/client#web-version-8
 (defn get-token> [public-vapid-key]
   (log ::get-token>)
-  (-> (messaging)
-      (.getToken public-vapid-key)
-      (.catch (fn [error]
-                (log ::get-token>--error
-                     :error error)
-                nil))))
+  (if-let [messaging (messaging)]
+    (-> messaging
+        (.getToken public-vapid-key)
+        (.catch (fn [error]
+                  (log ::get-token>--error
+                       :error error)
+                  nil)))
+    (u/resolve> nil)))
