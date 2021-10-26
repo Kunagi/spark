dfa5ae80f26d444effbf3c926836e27af2fd2412
Witoslaw Koczewski <wi@koczewski.de>
2021-10-26 12:33:54 +0200

    utils/assert

--- src/spark/utils.clj
+++ src/spark/utils.clj
@@ -1,17 +1,18 @@
 (ns spark.utils
   (:refer-clojure :exclude [assert])
   (:require
-   [clojure.pprint :refer [pprint]]
-   ))
+   [clojure.pprint :refer [pprint]]))
 
 (defmacro assert [assertion & [message data]]
   (let [assertion-formated (with-out-str (pprint assertion))
         in-function (str (-> &env :fn-scope first :info :ns) "." (-> &env :fn-scope first :name))
         in-file (-> &env :ns :meta :file)
-        in-line (-> &env :line)
-        ]
+        in-line (-> &env :line)]
     `(let [result# ~assertion]
        (when-not result#
+         (spark.logging/log ::assert--failed
+                            :message ~message
+                            :data ~data)
          (throw (ex-info (str
                           (when ~message (str  ~message "\n"))
                           "Assertion failed.\n"
@@ -22,6 +23,5 @@
                                 :assertion-form '~assertion
                                 :in-function ~in-function
                                 :in-line ~in-line
-                                :in-file ~in-file
-                                ))))
+                                :in-file ~in-file))))
        result#)))
