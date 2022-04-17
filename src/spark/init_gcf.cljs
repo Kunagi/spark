6d8041f4c1e14ba01c8836e0ef4f34aaaaf69d42
Witoslaw Koczewski <wi@koczewski.de>
2022-04-17 19:39:33 +0200

    write gcf-tap.edn

--- src/spark/init_gcf.cljs
+++ src/spark/init_gcf.cljs
@@ -1,6 +1,8 @@
 (ns spark.init-gcf
   (:require
    [clojure.spec.alpha :as s]
+   [cljs.pprint :refer [pprint]]
+   ["fs" :as fs]
 
    ["@peculiar/webcrypto" :refer [Crypto]]
    ["firebase-admin" :as admin]
@@ -8,7 +10,24 @@
 
    [spark.env-config :as env-config]))
 
+(defonce TAPS (atom '()))
 
+(defn on-tap [value]
+  (swap! TAPS conj {:ts (-> (js/Date.) .getTime)
+                    :value value})
+  (fs/writeFile "../../src/gcf-tap.edn"
+                (with-out-str (pprint @TAPS))
+                (fn [])))
+(comment
+  (tap> {:hello "world 2"})
+  (on-tap "hello")
+  (on-tap "world"))
+
+(defn install-tap-capture []
+  (add-tap on-tap))
+
+(comment
+  (tap> "hello"))
 
 (defn initialize []
 
@@ -19,8 +38,7 @@
   ;; https://github.com/zelark/nano-id
   (set! js/crypto (Crypto.))
 
-
-  ;; Logging
+;; Logging
   (set! js/_spark_logger (-> functions .-logger))
 
   ;; Firebase
@@ -29,7 +47,8 @@
   (env-config/set! :firebase-storage (-> admin .storage))
 
   ;;
-  )
+  (when goog.DEBUG
+    (install-tap-capture)))
 
 (defonce initialized
   (do
