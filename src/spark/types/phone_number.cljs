3e953bd28dccc0c89966757020531cb7b5d8a767
Witoslaw Koczewski <wi@koczewski.de>
2022-04-29 01:19:58 +0200

    phone-number

--- src/spark/types/phone_number.cljs
+++ src/spark/types/phone_number.cljs
@@ -2,7 +2,8 @@
   (:require
    [clojure.string :as str]
    [spark.rct :refer [test>]]
-   [spark.utils :as u]))
+   [spark.utils :as u]
+   [clojure.spec.alpha :as s]))
 
 (def country-prefix--de "49")
 
@@ -10,11 +11,24 @@
   (when s
     (assert default-country-prefix-number)
     (let [s (str/trim s)
-          s (str/replace s " " "")
-          s (str/replace s "/" "")
-          s (str/replace s "-" "")
-          s (str/replace s "(" "")
-          s (str/replace s ")" "")
+
+          starts-with+? (str/starts-with? s "+")
+          s (if starts-with+?
+              (-> s (.substring 1))
+              s)
+
+          s (str/replace s #"[^0-9]" "")
+
+          s (if starts-with+?
+              (str "+" s)
+              s)
+
+          ;; s (str/replace s " " "")
+          ;; s (str/replace s "/" "")
+          ;; s (str/replace s "-" "")
+          ;; s (str/replace s "(" "")
+          ;; s (str/replace s ")" "")
+
           s (if (-> s (.startsWith "00"))
               (str/replace s "00" "+")
               s)
@@ -24,16 +38,16 @@
       s)))
 
 (test> T->e123-spaceless
- (def _tel "+4957519934900")
- (def _de "49")
+       (def _tel "+4957519934900")
+       (def _de "49")
 
- (assert (= _tel (->e123-spaceless "+49 5751 9934900" _de)))
- (assert (= _tel (->e123-spaceless "+49 5751-9934900" _de)))
- (assert (= _tel (->e123-spaceless "+49 5751/9934900" _de)))
+       (assert (= _tel (->e123-spaceless "+49 5751 9934900" _de)))
+       (assert (= _tel (->e123-spaceless "+49 5751-9934900" _de)))
+       (assert (= _tel (->e123-spaceless "+49 5751/9934900" _de)))
 
- (assert (= _tel (->e123-spaceless "05751 9934900" _de)))
- (assert (= _tel (->e123-spaceless "05751/9934900" _de)))
- (assert (= _tel (->e123-spaceless "057519934900" _de)))
- (assert (= _tel (->e123-spaceless "(05751) 9934900" _de)))
+       (assert (= _tel (->e123-spaceless "05751 9934900" _de)))
+       (assert (= _tel (->e123-spaceless "05751/9934900" _de)))
+       (assert (= _tel (->e123-spaceless "057519934900" _de)))
+       (assert (= _tel (->e123-spaceless "(05751) 9934900" _de)))
 
- )
+       (assert (= _tel (->e123-spaceless "+49.5751+99 und 34900" _de))))
