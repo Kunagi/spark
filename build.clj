869fd51f9a19fd466e310da97127cb7945029a26
Witoslaw Koczewski <wi@koczewski.de>
2022-05-11 10:32:48 +0200

    [build] releasing

--- build.clj
+++ build.clj
@@ -3,16 +3,18 @@
   (:require
    [clojure.tools.build.api :as b]
 
-   [kunagi.build.api :as kb]
-   [kunagi.mui.build :as mui]
+   [kunagi.build.api :as kb :refer [print-task print-done print-debug]]
 
+   [kunagi.mui.build :as mui]
    ))
 
-;; (defn update
-;;   [{:keys []}]
-;;   (mui/update-package-json!)
-;;   (kb/npm-reinstall!))
+(defn update
+  [{:keys []}]
+  (mui/update-package-json!))
 
 (defn release [{:keys []}]
-  (kb/release! {})
+  (kb/release-2 {:project 'kunagi-build})
+  (kb/release-2 {:project 'kunagi-utils})
+  (kb/release-2 {:project 'kunagi-mui})
+  (kb/release-2 {:project 'spark})
   )
